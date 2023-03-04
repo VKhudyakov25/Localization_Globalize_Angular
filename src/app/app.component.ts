4e38963c3f38d47686d46b5468355150ab94edb8
Vyacheslav Khudyakov <slavak521@MacBook-Air-Vyacheslav.local>
2023-03-04 11:32:33 +0600

    Implement intremediate version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,63 @@
 import { Component } from '@angular/core';
 
-import { Product, AppService } from './app.service';
-import DataSource from 'devextreme/data/data_source';
-import CustomStore from 'devextreme/data/custom_store';
+import 'devextreme/localization/globalize/number';
+import 'devextreme/localization/globalize/date';
+import 'devextreme/localization/globalize/currency';
+import 'devextreme/localization/globalize/message';
+
+import deMessages from 'devextreme/localization/messages/de.json';
+import ruMessages from 'devextreme/localization/messages/ru.json';
+
+import deCldrData from 'devextreme-cldr-data/de.json';
+import ruCldrData from 'devextreme-cldr-data/ru.json';
+import supplementalCldrData from 'devextreme-cldr-data/supplemental.json';
+
+import Globalize from 'globalize';
+import { Payment, Locale, AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [AppService],
+  preserveWhitespaces: true,
 })
 export class AppComponent {
-  products: Product[];
-  productsDataSource: DataSource;
-  myStore: CustomStore;
-  searchModeOption = 'contains';
-  searchExprOption: any = 'Name';
-  searchTimeoutOption = 200;
-  minSearchLengthOption = 0;
-  showDataBeforeSearchOption = false;
+  locales: Locale[];
+  locale: string;
+  payments: Payment[];
 
-  constructor(service: AppService) {
-    this.products = service.getProducts();
-    this.myStore = new CustomStore({
-      key: 'id',
-      loadMode: 'raw',
-      load: () => {
-        return service.getProducts();
-      },
-      insert: (val) => {
-        return service.insertProduct(val);
-      },
-    });
-    this.productsDataSource = new DataSource({
-      store: this.myStore,
-    });
+  formatMessage = Globalize.formatMessage.bind(Globalize);
+
+  constructor(private service: AppService) {
+    this.locale = this.getLocale();
+    this.payments = service.getPayments();
+    this.locales = service.getLocales();
+
+    // this.initGlobalize();
+    Globalize.loadMessages(service.getDictionary());
+    Globalize.locale(this.locale);
+    Globalize.load(supplementalCldrData, deCldrData, ruCldrData);
+    Globalize.loadMessages(deMessages);
+    Globalize.loadMessages(ruMessages);
   }
 
-  addCustomItem = (data: any) => {
-    if (!data.text) {
-      data.customItem = null;
-      return;
-    }
-    let productIds: any;
-    this.myStore.load().then((e: any) => {
-      productIds = e.map((item: Product) => item.id);
-    });
-    let incrementedId = Math.max.apply(null, productIds) + 1;
-    let newItem = {
-      Name: data.text,
-      id: incrementedId,
-    };
-    this.myStore
-      .insert(newItem)
-      .then(() => this.productsDataSource.load())
-      .then(() => newItem)
-      .catch((error) => {
-        throw error;
-      });
-  };
+  initGlobalize() {
+    // Globalize.load(supplementalCldrData, deCldrData, ruCldrData);
+    // Globalize.loadMessages(deMessages);
+    // Globalize.loadMessages(ruMessages);
+    // Globalize.loadMessages(this.service.getDictionary());
+  }
+  changeLocale(data: any) {
+    console.log('data :>> ', data);
+    this.setLocale(data.value);
+    parent.document.location.reload();
+  }
+  getLocale() {
+    const storageLocale = sessionStorage.getItem('locale');
+    return storageLocale != null ? storageLocale : 'en';
+  }
+  setLocale(savingLocale: any) {
+    sessionStorage.setItem('locale', savingLocale);
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,42 +1,145 @@
 import { Injectable } from '@angular/core';
 
-export class Product {
-  Name: string;
-  id: number;
+export class Payment {
+  PaymentId: number;
+  ContactName: string;
+  CompanyName: string;
+  Amount: number;
+  PaymentDate: string;
 }
-let products: Product[] = [
-  { Name: 'HD Video Player', id: 0 },
-  { Name: 'SuperHD Video Player', id: 1 },
-  { Name: 'SuperPlasma 50', id: 2 },
-  { Name: 'SuperLED 50', id: 3 },
-  { Name: 'SuperLED 42', id: 4 },
-  { Name: 'SuperLCD 55', id: 5 },
-  { Name: 'SuperLCD 42', id: 6 },
-  { Name: 'SuperPlasma 65', id: 7 },
-  { Name: 'SuperLCD 70', id: 8 },
-  { Name: 'Projector Plus', id: 9 },
-  { Name: 'Projector PlusHT', id: 10 },
-  { Name: 'ExcelRemote IR', id: 11 },
-  { Name: 'ExcelRemote Bluetooth', id: 12 },
-  { Name: 'ExcelRemote IP', id: 13 },
+
+export class Locale {
+  name: string;
+  value: string;
+}
+
+const locales: Locale[] = [
+  { name: 'English', value: 'en' },
+  { name: 'Deutsch', value: 'de' },
+  { name: 'Русский', value: 'ru' },
 ];
+const payments: Payment[] = [
+  {
+    PaymentId: 1,
+    ContactName: 'Nancy Davolio',
+    CompanyName: 'Premier Buy',
+    Amount: 1740,
+    PaymentDate: '2013/01/06',
+  },
+  {
+    PaymentId: 2,
+    ContactName: 'Andrew Fuller',
+    CompanyName: 'ElectrixMax',
+    Amount: 850,
+    PaymentDate: '2013/01/13',
+  },
+  {
+    PaymentId: 3,
+    ContactName: 'Janet Leverling',
+    CompanyName: 'Video Emporium',
+    Amount: 2235,
+    PaymentDate: '2013/01/07',
+  },
+  {
+    PaymentId: 4,
+    ContactName: 'Margaret Peacock',
+    CompanyName: 'Screen Shop',
+    Amount: 1965,
+    PaymentDate: '2013/01/03',
+  },
+  {
+    PaymentId: 5,
+    ContactName: 'Steven Buchanan',
+    CompanyName: 'Braeburn',
+    Amount: 880,
+    PaymentDate: '2013/01/10',
+  },
+  {
+    PaymentId: 6,
+    ContactName: 'Michael Suyama',
+    CompanyName: 'PriceCo',
+    Amount: 5260,
+    PaymentDate: '2013/01/17',
+  },
+  {
+    PaymentId: 7,
+    ContactName: 'Robert King',
+    CompanyName: 'Ultimate Gadget',
+    Amount: 2790,
+    PaymentDate: '2013/01/21',
+  },
+  {
+    PaymentId: 8,
+    ContactName: 'Laura Callahan',
+    CompanyName: 'EZ Stop',
+    Amount: 3140,
+    PaymentDate: '2013/01/01',
+  },
+  {
+    PaymentId: 9,
+    ContactName: 'Anne Dodsworth',
+    CompanyName: 'Clicker',
+    Amount: 6175,
+    PaymentDate: '2013/01/24',
+  },
+  {
+    PaymentId: 10,
+    ContactName: 'Clark Morgan',
+    CompanyName: 'Store of America',
+    Amount: 4575,
+    PaymentDate: '2013/01/11',
+  },
+];
+
+export class PaymentView {
+  Number: string;
+  Contact: string;
+  Company: string;
+  Amount: string;
+  PaymentDate: string;
+  BtnText: string;
+}
+
+export class Dictionary {
+  [key: string]: PaymentView;
+}
+
+const dictionary: Dictionary = {
+  en: {
+    Number: 'Number',
+    Contact: 'Contact',
+    Company: 'Company',
+    Amount: 'Amount',
+    PaymentDate: 'Payment Date',
+    BtnText: 'Click me',
+  },
+  de: {
+    Number: 'Nummer',
+    Contact: 'Ansprechpartner',
+    Company: 'Firma',
+    Amount: 'Betrag',
+    PaymentDate: 'Zahlungsdatum',
+    BtnText: 'Klick mich an',
+  },
+  ru: {
+    Number: 'Номер',
+    Contact: 'Имя',
+    Company: 'Организация',
+    Amount: 'Сумма',
+    PaymentDate: 'Дата оплаты',
+    BtnText: 'Нажми на меня',
+  },
+};
 
 @Injectable()
 export class AppService {
-  getProducts(): any {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(products);
-      }, 1000);
-    });
+  getPayments(): any {
+    return payments;
   }
-
-  insertProduct(product: Product): any {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        products.push(product);
-        resolve(products);
-      }, 1000);
-    });
+  getLocales(): any {
+    return locales;
+  }
+  getDictionary(): any {
+    return dictionary;
   }
 }
